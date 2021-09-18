8cf6178232827e62ffb4acef00a625d1211a1585
Fungucide <wc.fung@mail.utoronto.ca>
2021-09-18 16:44:11 -0400

    User page displays postings and groups

--- Frontend/src/components/User.jsx
+++ Frontend/src/components/User.jsx
@@ -1,6 +1,7 @@
 import Axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router";
+import { Link } from "react-router-dom";
 import { BASE_URL } from "../Constants";
 import Loading from "./Loading";
 
@@ -14,22 +15,48 @@ const User = () => {
     Axios.get(BASE_URL + "/api/user", {
       params: { username: username },
     }).then((data) => {
+      console.log(data.data);
       setUserData(data.data);
       setLoading(false);
     });
-  });
+  }, []);
   return (
     <>
       {loading ? (
         <Loading />
       ) : (
         <>
-          <h1 className="text-4xl text-white mb-4 border-b-2">
+          <h1 className="text-4xl text-white mb-4 font-bold">
             {userData.username}
           </h1>
-          <div className="text-white border border-black rounded-xl p-4">
-            {userData}
-          </div>
+          <h2 className="text-2xl text-white border-b-2">Posts</h2>
+          {userData.postings.map((posting) => (
+            <>
+              <Link
+                to={`/posting/${posting.postid}`}
+                className="text-xl text-white"
+              >
+                {posting.title}
+              </Link>
+              <div className="text-white border border-black rounded-xl p-4">
+                {posting.postbody}
+              </div>
+            </>
+          ))}
+          <h2 className="text-2xl text-white border-b-2">Groups</h2>
+          {userData.groups.map((posting) => (
+            <>
+              <Link
+                to={`/posting/${posting.postid}`}
+                className="text-xl text-white"
+              >
+                {posting.title}
+              </Link>
+              <div className="text-white border border-black rounded-xl p-4">
+                {posting.postbody}
+              </div>
+            </>
+          ))}
         </>
       )}
     </>
