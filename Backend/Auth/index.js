62d6559220e17b5984e94005f652afaae5d8d7b4
Fungucide <wc.fung@mail.utoronto.ca>
2021-09-17 22:49:54 -0400

    Added auth login and account creation

---
+++ Backend/Auth/index.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const { TransactionWraper } = require("../DatabaseUtil");
+const {
+  HashPassword,
+  GetUserRow,
+  ComparePassword,
+  CreateJWTs,
+} = require("./util");
+
+const router = express.Router();
+
+const POST_SQL_STATEMENT =
+  "INSERT INTO gotcrewmates.users (username, hash) VALUES ($1, $2)";
+
+// Create a new user
+router.post("/user", async (req, res) => {
+  // Gether data from req
+  try {
+    const { username, password } = req.body;
+    // Make sure request has required data
+    if (!username || !password) {
+      res
+        .status(400)
+        .send(`Missing ${username && "username"} ${password && "passowrd"}`);
+      return;
+    }
+    const hash = HashPassword(password);
+    TransactionWraper((client) =>
+      client.query(POST_SQL_STATEMENT, [username, hash])
+    )
+      .then(() => {
+        res.sendStatus(201);
+      })
+      .catch((e) => {
+        // Catch duplicate usernames here
+        if (e.code === "23505") {
+          res.status(400).send("Username already exists");
+          return;
+        }
+        console.error(e);
+        res.sendStatus(500);
+      });
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+// Login route
+router.post("/session", async (req, res) => {
+  // Request should have a username and password
+  try {
+    const { username, password } = req.body;
+    // Make sure request has required data
+    if (!username || !password) {
+      res
+        .status(400)
+        .send(`Missing ${username && "username"} ${password && "passowrd"}`);
+      return;
+    }
+    const { hash, userid: userID } = await GetUserRow(username);
+    const success = ComparePassword(password, hash);
+    if (success) {
+      const [accessToken, refreshToken] = CreateJWTs(userID, username);
+      res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        sameSite: "strict",
+      });
+      res.status(201).send(accessToken);
+    } else {
+      res.sendStatus(401);
+    }
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+module.exports = router;
