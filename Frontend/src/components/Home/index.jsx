b5d1aeb7b21ea6f5a695a654230c22db4d1d5a8e
Fungucide <wc.fung@mail.utoronto.ca>
2021-09-18 17:49:22 -0400

    added home page

---
+++ Frontend/src/components/Home/index.jsx
@@ -0,0 +1,57 @@
+import Axios from "axios";
+import { useState, useEffect } from "react";
+import { Link, useParams, useHistory, useLocation } from "react-router-dom";
+import { BASE_URL } from "../../Constants";
+import Cookies from "js-cookie";
+import Loading from "../Loading";
+import SmallPosting from "./SmallPosting";
+
+const Home = () => {
+  const history = useHistory();
+  const { id } = useParams();
+
+  const [loading, setLoading] = useState(true);
+  const [postingData, setPostingData] = useState({});
+  const [pageNumber, setPageNumber] = useState(1);
+
+  useEffect(() => {
+    const token = Cookies.get("token");
+    if (token) {
+      Axios.get(BASE_URL + "/api/posting", {
+        params: { pageNumber: pageNumber },
+        headers: { Authorization: `Bearer ${token}` },
+      })
+        .then((data) => {
+          console.log(data.data);
+          setPostingData(data.data);
+          setLoading(false);
+        })
+        .catch((e) => {
+          if (e.response.status === 401) {
+            history.push("/login");
+          }
+        });
+    } else {
+      history.push("/login");
+    }
+  }, [id, history]);
+
+  return (
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <div>
+            {postingData.map((post) => (
+              <SmallPosting posting={post} />
+            ))}
+          </div>
+          
+        </>
+      )}
+    </>
+  );
+};
+
+export default Home;
