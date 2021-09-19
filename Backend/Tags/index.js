01251a8e9070fb354ed963a22cb69b437abf9623
vincent <54919042+vince212121@users.noreply.github.com>
2021-09-18 20:41:33 -0400

    added POST for tags

--- Backend/Tags/index.js
+++ Backend/Tags/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const { TransactionWraper } = require("../DatabaseUtil");
 const router = express.Router();
 
-const POST_SQL_STATEMENT = "INSERT INTO gotcrewmates.tags (tagname) VALUES $1;";
+const POST_SQL_STATEMENT = "INSERT INTO gotcrewmates.tags (tagname) VALUES ($1);";
 
 // used to GET all tags
 // get arguements for text query
@@ -24,71 +24,33 @@ router.get("/tags", async (req, res) => {
     }
 
     if (searchParam && pageNumber >= 2) {
-        additionalStatement = " WHERE lower(tagname) LIKE $1 OFFSET ($2 * 20) ROWS LIMIT 20";
-        searchParam = "%"+ req.query.searchParameter + "%";
-        pageNumber -= 1;
-        queryParamenter = [searchParam, pageNumber];
+      additionalStatement =
+        " WHERE lower(tagname) LIKE $1 OFFSET ($2 * 20) ROWS LIMIT 20";
+      searchParam = "%" + req.query.searchParameter + "%";
+      pageNumber -= 1;
+      queryParamenter = [searchParam, pageNumber];
     } else if (searchParam) {
-        additionalStatement = " WHERE lower(tagname) LIKE $1 LIMIT 20";
-        searchParam = "%"+ req.query.searchParameter + "%";
-        queryParamenter = [searchParam];
+      additionalStatement = " WHERE lower(tagname) LIKE $1 LIMIT 20";
+      searchParam = "%" + req.query.searchParameter + "%";
+      queryParamenter = [searchParam];
     } else if (pageNumber >= 2) {
-        pageNumber -= 1;
-        additionalStatement = " OFFSET ($1 * 20) ROWS LIMIT 20";
-        queryParamenter = [pageNumber];
+      pageNumber -= 1;
+      additionalStatement = " OFFSET ($1 * 20) ROWS LIMIT 20";
+      queryParamenter = [pageNumber];
     } else if (pageNumber == 1) {
-        additionalStatement = " LIMIT 20";
+      additionalStatement = " LIMIT 20";
+    } else if (isNaN(pageNumber) || pageNumber <= 0) {
+      res.status(400).send(`Invalid page number`);
+      return;
     }
-     else if (isNaN(pageNumber) || pageNumber <= 0) {
-        res.status(400).send(`Invalid page number`);
-        return;
-    }
-
-    // used for searching
-    // if (searchParam) {
-    //   additionalStatement += " WHERE lower(tagname) LIKE $1%";
-    //   queryParamenter = [searchParam.toLowerCase()];
-
-    //   console.log("in search param");
-    //   console.log("statement: " + getSqlStatement + additionalStatement + ";");
-    //   console.log("query " + queryParamenter);
-    //   console.log("search: " + searchParam.toLowerCase());
-
-    //   // getting a certian amount of tags at a time
-    // //   if (pageNumber >= 2) {
-    // //     additionalStatement += " OFFSET ($2 * 20) ROWS";
-    // //     queryParamenter = [searchParam.toLowerCase(), pageNumber];
-
-    // //     console.log("in pg number");
-    // //     console.log(
-    // //       "statement: " + getSqlStatement + additionalStatement + ";"
-    // //     );
-    // //     console.log("query " + queryParamenter);
-    // //     console.log("search: " + searchParam.toLowerCase());
-    // //   }
-    // } else {
-    //   // getting a certian amount of tags at a time
-    //   if (isNaN(pageNumber) || pageNumber <= 0) {
-    //     res.status(400).send(`Invalid page number`);
-    //     return;
-    //   } else if (pageNumber >= 2) {
-    //     additionalStatement += " OFFSET ($1 * 20) ROWS";
-    //     queryParamenter = [pageNumber];
-    //   }
-    // }
 
     TransactionWraper((client) =>
       client.query(getSqlStatement + additionalStatement + ";", queryParamenter)
     )
       .then((result) => {
-        console.log("made it in");
-        console.log(
-          "statement: " + getSqlStatement + additionalStatement + ";"
-        );
         res.status(200).send(result.rows);
       })
       .catch((e) => {
-        console.log(e);
         res.sendStatus(500);
       });
   } catch (error) {
@@ -112,25 +74,19 @@ router.post("/tags", async (req, res) => {
       return;
     }
 
-    // TransactionWraper((client) =>
-    //   client.query(
-    //     POST_SQL_STATEMENT,
-    //     queryParamenter
-    //   )
-    // )
-    //   .then((result) => {
-    //     if (!postID || result.rows.length > 0) {
-    //       res.status(200).send(result.rows);
-    //     } else res.sendStatus(400);
-    //   })
-    //   .catch((e) => {
-    //     res.sendStatus(500);
-    //   });
+    TransactionWraper((client) => client.query(POST_SQL_STATEMENT, [tagName]))
+      .then(() => {
+        res.sendStatus(200);
+      })
+      .catch((e) => {
+        res.sendStatus(500);
+      });
   } catch (error) {
     res.sendStatus(500);
   }
 });
 
 // getting posts by tag
+
 
 module.exports = router;
