729bd984b12876f8edb75dbf0a7ef2e6d8546639
Fungucide <wc.fung@mail.utoronto.ca>
2021-09-19 04:17:19 -0400

    fixed searchbar styling

--- Frontend/src/components/header.jsx
+++ Frontend/src/components/header.jsx
@@ -33,67 +33,71 @@ const Header = () => {
     }
   }, [token]);
 
+  const HomeSide = () => (
+    <Link
+      to="/"
+      className="home bg-header text-3xl font-bold p-4 flex flex-row whitespace-nowrap"
+    >
+      <img src="/favicon-32x32.png" alt="Pepe" className="pepe-ico" />
+      Got Crew Mates?
+    </Link>
+  );
+
+  const UnAuthedComponent = () => (
+    <>
+      <Link to="/login" className="text-2xl mr-4 p-1 bg-lightblue rounded-lg">
+        Login
+      </Link>
+      <Link to="/signup" className="text-2xl mr-4 p-1 bg-lightblue rounded-lg">
+        Sign Up
+      </Link>
+    </>
+  );
+
+  const AuthedComponent = () => (
+    <div className="h-full flex-1 flex flex-row items-center mx-16">
+      <BrowseTags />
+      <div className="profile h-full flex items-center">
+        <Link to={`/user/${username}`} className="text-2xl mx-4 p-1">
+          {username}
+        </Link>
+        <div className="dropdown absolute hidden top-full right-16">
+          <div className="flex flex-col">
+            <button
+              className="bg-lightblue p-2 text-2xl border-b-2 border-lightlightblue"
+              onClick={() => {
+                Cookies.remove("token");
+                setToken(undefined);
+              }}
+            >
+              Logout
+            </button>
+            <Link to="/new-posting" className="bg-lightblue p-2 text-2xl">
+              New Posting
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
   return (
     <>
-      <header className="w-full flex flex-row bg-lightlightblue sticky">
-        <Link
-          to="/"
-          className="home bg-header text-3xl font-bold pl-2 flex flex-row whitespace-nowrap"
-        >
-          <img src="/favicon-32x32.png" alt="Pepe" className="pepe-ico" />
-          Got Crew Mates?
-        </Link>
-        <div className="float-left">
-          <SearchBar />
-        </div>
-        <div className="h-full ml-auto float-left top-0">
-          {auth ? (
-            <div className="h-max flex flex-row">
-              <BrowseTags />
-              <div className="profile">
-                <Link to={`/user/${username}`} className="text-2xl mr-4 p-1">
-                  {username}
-                </Link>
-                <div className="dropdown absolute hidden float-right">
-                  <div className="flex flex-col">
-                    <button
-                      className="bg-lightblue mr-4 p-2 text-2xl"
-                      onClick={() => {
-                        Cookies.remove("token");
-                        setToken(undefined);
-                      }}
-                    >
-                      Logout
-                    </button>
-                    <Link to="/new-posting">New Posting</Link>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ) : (
-            <>
-              <Link
-                to="/login"
-                className="text-2xl mr-4 p-1 bg-lightblue rounded-lg"
-              >
-                Login
-              </Link>
-              <Link
-                to="/signup"
-                className="text-2xl mr-4 p-1 bg-lightblue rounded-lg"
-              >
-                Sign Up
-              </Link>
-            </>
-          )}
+      <header className="w-full bg-lightlightblue sticky">
+        <div className="relative flex flex-row w-full">
+          <HomeSide />
+          <div className="absolute w-1/4 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <SearchBar />
+          </div>
+          <div className="h-max ml-auto float-left">
+            {auth ? <AuthedComponent /> : <UnAuthedComponent />}
+          </div>
         </div>
       </header>
       <style jsx>
         {`
           .profile:hover .dropdown {
             display: block;
-            top: 90%;
-            right: 0px;
           }
 
           .home:hover .pepe-ico {
