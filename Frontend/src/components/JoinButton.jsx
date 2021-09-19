28044792a7fde973c40da7d55de8d5c27013cd8c
Bownayr <70776863+Bownayr@users.noreply.github.com>
2021-09-19 04:17:29 -0400

    Set up button for joining

---
+++ Frontend/src/components/JoinButton.jsx
@@ -0,0 +1,61 @@
+import { useState, useEffect } from "react";
+import Axios from "axios";
+import { BASE_URL } from "../Constants";
+import { useHistory, useLocation } from "react-router-dom";
+import Cookies from "js-cookie";
+
+const JoinButton = ({ post }) => {
+  const history = useHistory();
+  const location = useLocation();
+  const [buttonData, setButtonData] = useState([]);
+  const [token, setToken] = useState();
+  const [text, setText] = useState("Join Group");
+  const [disabled,setDisabled] = useState(false);
+
+  useEffect(() => {
+    const cookieToken = Cookies.get("token");
+    if (!cookieToken) {
+      history.push(`/login?redirect=${location.pathname}`);
+    }
+    setToken(cookieToken);
+  }, []);
+
+  const onClick = (e) => {
+    console.log(e);
+    setDisabled(true);
+    setText("Waiting...");
+    Axios.post(
+      BASE_URL + "/api/postdetails/",
+      {
+        postID: post.postid,
+      },
+      {
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    )
+      .then((info) => {
+        setButtonData(info.data);
+        setText("Joined group");
+        setDisabled(false);
+      })
+      .catch((e) => {
+        if (e.response.status === 400) {
+          setDisabled(false);
+        }
+      });
+  };
+  return (
+    <>
+      <div>
+        <button
+          className="click-change bg-btn-50 disabled:opacity-50"
+          onClick={onClick}
+          disabled={disabled}
+        >
+          {text}
+        </button>
+      </div>
+    </>
+  );
+};
+export default JoinButton;
